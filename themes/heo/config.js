95ef106b463a95d61cb51da51d37c674c6b08753
dreameutopia <13077995107@163.com>
2024-10-02 01:45:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '访问我的github', url: 'https://github.com/dreameutopia' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -19,11 +19,11 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '与日常生活',
+  HEO_HERO_TITLE_3: 'dreameutopia.cn',
+  HEO_HERO_TITLE_4: '扬帆起航',
+  HEO_HERO_TITLE_5: 'DreamEutopia 崭新的开始',
+  HEO_HERO_TITLE_LINK: 'https://github.com/dreameutopia',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
